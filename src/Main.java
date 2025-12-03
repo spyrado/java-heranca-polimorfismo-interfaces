1f00b1d653182b18052dfcd65eb28294f4d466d6
Nicolas Guilherme Matheus <nicolas.gmop@gmail.com>
2025-12-02 22:16:28 -0300

    feat: deixando variaveis genericas (Funcionario) para aplicar cast no gerente

--- src/Main.java
+++ src/Main.java
@@ -4,12 +4,12 @@ import model.Gerente;
 
 public class Main {
   public static void main(String[] args) {
-    Gerente gerente = new Gerente("Mario", 15000);
+    Funcionario gerente = new Gerente("Mario", 15000);
     gerente.exibirInformacoes();
     gerente.reajustarSalario(2);
-    gerente.setBonus(1000);
+    ((Gerente) gerente).setBonus(1000);
 
-    Desenvolvedor desenvolvedor = new Desenvolvedor("Carla", 12000, "Backend Java");
+    Funcionario desenvolvedor = new Desenvolvedor("Carla", 12000, "Backend Java");
     desenvolvedor.exibirInformacoes();
 
   }
